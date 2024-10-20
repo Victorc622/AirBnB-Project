762b188a3bbae4fa6ab5b31e81aa9f8936f0d990
Victor Cai <victorcai622@gmail.com>
2024-10-20 17:39:06 -0400

    fixed errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,39 +3,64 @@ import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
+import Spots from './components/Spots';
+import SpotDetails from './components/SpotDetails';
+import CreateSpot from './components/SpotForms/CreateSpot.jsx';
+import EditSpot from './components/SpotForms/EditSpot.jsx';
+import ManageSpots from './components/Spots/ManageSpots.jsx';
 
 function Layout() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
+	const dispatch = useDispatch();
+	const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => {
-      setIsLoaded(true)
-    });
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(sessionActions.restoreUser()).then(() => {
+			setIsLoaded(true);
+		});
+	}, [dispatch]);
 
-  return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && <Outlet />}
-    </>
-  );
+	return (
+		<main>
+			<Navigation isLoaded={isLoaded} />
+			{isLoaded && <Outlet />}
+		</main>
+	);
 }
 
 const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <h1>Welcome!</h1>
-      }
-    ]
-  }
+	{
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Spots />,
+			},
+			{
+				path: '/spots',
+				element: <Spots />,
+			},
+			{
+				path: '/spots/:id',
+				element: <SpotDetails />,
+			},
+			{
+				path: '/spots/new',
+				element: <CreateSpot />,
+			},
+			{
+				path: '/spots/manage',
+				element: <ManageSpots />,
+			},
+			{
+				path: '/spots/:id/edit',
+				element: <EditSpot />,
+			},
+		],
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
